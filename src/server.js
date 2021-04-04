e7a03728b3e7a3e8e6604f255e4d58d8b0ae1f1b
jensettl <ettljens@gmail.com>
2021-04-04 11:31:35 +0200

    Update server.js
    Server Struktur aufgebaut, Funktion bisher nur mit Debugging Methodenrumpf

--- src/server.js
+++ src/server.js
@@ -1,2 +1,18 @@
 const grpc = require("grpc");
 const protoLoader = require("@grpc/proto-loader");
+const packageDef = protoLoader.loadSync("stockExchange.proto", {});
+const grpcObject = grpc.loadPackageDefinition(packageDef);
+const stockExchangePackage = grpcObject.stockExchangePackage;
+
+const server = new grpc.Server();
+server.bind("0.0.0.0:50051",grpc.ServerCredentials.createInsecure());
+
+server.addService(stockExchangePackage.CustomerService.service, 
+    {
+        "GetStockExchangeInfo": GetStockExchangeInfo,
+    });
+server.start();
+
+function GetStockExchangeInfo (call, callback){
+    console.log(call);
+}
