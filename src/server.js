e1fa80f095e45050d5a45a6e7ae91f6717036ad3
jensettl <ettljens@gmail.com>
2021-04-06 10:59:38 +0200

    added: schreibenden RPC Call

--- src/server.js
+++ src/server.js
@@ -9,10 +9,24 @@ server.bind("0.0.0.0:50051",grpc.ServerCredentials.createInsecure());
 
 server.addService(stockExchangePackage.CustomerService.service, 
     {
+        "createStockExchange": createStockExchange,
         "getStockExchangeInfo": getStockExchangeInfo,
-        "getTrades": getTrades
+        "getTrades": getTrades   
     });
 server.start();
+
+const tradeItems = require("./tradeItems.json")
+
+function createStockExchange(call, callback) {
+    const tradeItems = {
+        "id": tradeItems.length + 1,
+        "name": call.request.name,
+        "typ": call.request.typ,
+        "wert": call.request.wert
+    }
+    tradeItems.push(tradeItems);
+    callback(null, tradeItem);
+}
 
 const tradeItems = require("./tradeItems.json")
 
@@ -21,8 +35,9 @@ function getStockExchangeInfo (call, callback){
     
     if(call.request.tradeNr > tradeItems.length) 
     {
-        callback(new Error("Argument of type number is expected."));
-        return;
+        console.error("Börsengeschäft mit der angegebenen Trade-Nr existiert nicht!")
+        // callback(new Error("Argument of type number is expected."));
+        // return;
     }
         
     callback(null, tradeItems[call.request.tradeNr - 1]);
